da84e662ef3b7c3d97ea8eb18d1bfefe7e9d3d20
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-10 19:04:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,6 @@ def assign_rooms(speakers)
   greet = []
   speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
   return greet
-  
 end
 
 def printer
