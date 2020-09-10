3abbea8cd3e9fedc95170366c7c818e1e56c3537
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-10 19:04:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,10 @@ def batch_badge_creator(attendees)
   attendees.map{ |attendee| "Hello, my name is #{attendee}."}
 end
 
-def assign_rooms(attendees, number)
+def assign_rooms(speakers)
+  greet = []
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
+  return greet
   
 end
 
