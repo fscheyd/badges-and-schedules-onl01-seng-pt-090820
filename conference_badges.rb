7dee753a7924bb14a4bd3a35f03424ea3325d71a
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-10 18:45:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees, number)
-  attendees.map{|attendee|
+  attendees.map{|attendee| "Hello, #{attendees}! You'll be assigned to room !"
 end
 
 def printer
